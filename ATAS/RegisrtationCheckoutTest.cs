46f1275f655471cb11a5121518e9e52ca52499e6
Maksim-R <rmb89@mail.ru>
2024-11-11 01:07:04 +0500

    Проверка, что текущий URL соответствует странице тарифов

--- ATAS/RegisrtationCheckoutTest.cs
+++ ATAS/RegisrtationCheckoutTest.cs
@@ -12,6 +12,12 @@ namespace ATAS.Tests
             // Переход на страницу тарифов
             GoToUrl(BaseUrl.Tariff.Tariffs);
 
+            // Явное ожидание загрузки страницы тарифов
+            wait.Until(driver => driver.Url.Contains("https://my.trade-with.me/tariffs"));
+
+            // Проверка, что текущий URL соответствует странице тарифов
+            Assert.IsTrue(driver.Url.Contains("https://my.trade-with.me/tariffs"), "Открыта неверная страница для теста авторизации.");
+
         }
     }
 }
